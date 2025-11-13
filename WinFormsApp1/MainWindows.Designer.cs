568f589bd3bae9c7ce5c4799efd9f5e9ece8e2c0
alkanty <kyriann-pro@hotmail.fr>
2025-11-13 15:17:07 +0100

    test

--- WinFormsApp1/MainWindows.Designer.cs
+++ WinFormsApp1/MainWindows.Designer.cs
@@ -32,6 +32,15 @@
             button_NewTemp = new Button();
             buttonEdition = new Button();
             ConfigurationPanel = new Panel();
+            tabControl_Parametre = new TabControl();
+            tabpage_General = new TabPage();
+            button_Vlan = new Button();
+            label_Vlan = new Label();
+            textBox_Vlan = new TextBox();
+            tabPage_Vlan = new TabPage();
+            button_Hostname = new Button();
+            label_Hostname = new Label();
+            textbox_Hostname = new TextBox();
             groupBox1 = new GroupBox();
             label_Version = new Label();
             label_Modele = new Label();
@@ -44,20 +53,11 @@
             button_Save = new Button();
             label_Path = new Label();
             checkBox1 = new CheckBox();
-            tabControl_Parametre = new TabControl();
-            tab_page_general = new TabPage();
-            tabPage2 = new TabPage();
-            button_Hostname = new Button();
-            label_Hostname = new Label();
-            textbox_Hostname = new TextBox();
-            button1 = new Button();
-            label1 = new Label();
-            textBox1 = new TextBox();
             ConfigurationPanel.SuspendLayout();
-            groupBox1.SuspendLayout();
             tabControl_Parametre.SuspendLayout();
-            tab_page_general.SuspendLayout();
-            tabPage2.SuspendLayout();
+            tabpage_General.SuspendLayout();
+            tabPage_Vlan.SuspendLayout();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // button_NewTemp
@@ -91,6 +91,100 @@
             ConfigurationPanel.Name = "ConfigurationPanel";
             ConfigurationPanel.Size = new Size(522, 591);
             ConfigurationPanel.TabIndex = 2;
+            // 
+            // tabControl_Parametre
+            // 
+            tabControl_Parametre.AccessibleName = "";
+            tabControl_Parametre.Controls.Add(tabpage_General);
+            tabControl_Parametre.Controls.Add(tabPage_Vlan);
+            tabControl_Parametre.Location = new Point(18, 283);
+            tabControl_Parametre.Name = "tabControl_Parametre";
+            tabControl_Parametre.SelectedIndex = 0;
+            tabControl_Parametre.Size = new Size(480, 283);
+            tabControl_Parametre.TabIndex = 12;
+            // 
+            // tabpage_General
+            // 
+            tabpage_General.Controls.Add(label_Hostname);
+            tabpage_General.Controls.Add(button_Hostname);
+            tabpage_General.Controls.Add(textbox_Hostname);
+            tabpage_General.Location = new Point(4, 34);
+            tabpage_General.Name = "tabpage_General";
+            tabpage_General.Padding = new Padding(3);
+            tabpage_General.Size = new Size(472, 245);
+            tabpage_General.TabIndex = 0;
+            tabpage_General.Text = "General";
+            tabpage_General.UseVisualStyleBackColor = true;
+            // 
+            // button_Vlan
+            // 
+            button_Vlan.Location = new Point(356, 38);
+            button_Vlan.Margin = new Padding(4);
+            button_Vlan.Name = "button_Vlan";
+            button_Vlan.Size = new Size(51, 36);
+            button_Vlan.TabIndex = 13;
+            button_Vlan.Text = "Press";
+            button_Vlan.UseVisualStyleBackColor = true;
+            // 
+            // label_Vlan
+            // 
+            label_Vlan.AutoSize = true;
+            label_Vlan.Location = new Point(87, 44);
+            label_Vlan.Margin = new Padding(2, 0, 2, 0);
+            label_Vlan.Name = "label_Vlan";
+            label_Vlan.Size = new Size(57, 25);
+            label_Vlan.TabIndex = 11;
+            label_Vlan.Text = "VLAN";
+            // 
+            // textBox_Vlan
+            // 
+            textBox_Vlan.Location = new Point(159, 41);
+            textBox_Vlan.Margin = new Padding(2);
+            textBox_Vlan.Name = "textBox_Vlan";
+            textBox_Vlan.Size = new Size(188, 31);
+            textBox_Vlan.TabIndex = 12;
+            // 
+            // tabPage_Vlan
+            // 
+            tabPage_Vlan.Controls.Add(button_Vlan);
+            tabPage_Vlan.Controls.Add(label_Vlan);
+            tabPage_Vlan.Controls.Add(textBox_Vlan);
+            tabPage_Vlan.Location = new Point(4, 34);
+            tabPage_Vlan.Name = "tabPage_Vlan";
+            tabPage_Vlan.Padding = new Padding(3);
+            tabPage_Vlan.Size = new Size(472, 245);
+            tabPage_Vlan.TabIndex = 1;
+            tabPage_Vlan.Text = "VLAN";
+            tabPage_Vlan.UseVisualStyleBackColor = true;
+            // 
+            // button_Hostname
+            // 
+            button_Hostname.Location = new Point(371, 36);
+            button_Hostname.Margin = new Padding(4);
+            button_Hostname.Name = "button_Hostname";
+            button_Hostname.Size = new Size(51, 36);
+            button_Hostname.TabIndex = 10;
+            button_Hostname.Text = "button1";
+            button_Hostname.UseVisualStyleBackColor = true;
+            button_Hostname.Click += button_Hostname_Click;
+            // 
+            // label_Hostname
+            // 
+            label_Hostname.AutoSize = true;
+            label_Hostname.Location = new Point(59, 41);
+            label_Hostname.Margin = new Padding(2, 0, 2, 0);
+            label_Hostname.Name = "label_Hostname";
+            label_Hostname.Size = new Size(102, 25);
+            label_Hostname.TabIndex = 8;
+            label_Hostname.Text = "Host Name";
+            // 
+            // textbox_Hostname
+            // 
+            textbox_Hostname.Location = new Point(170, 38);
+            textbox_Hostname.Margin = new Padding(2);
+            textbox_Hostname.Name = "textbox_Hostname";
+            textbox_Hostname.Size = new Size(188, 31);
+            textbox_Hostname.TabIndex = 9;
             // 
             // groupBox1
             // 
@@ -146,7 +240,7 @@
             // 
             comboBox_Version.FormattingEnabled = true;
             comboBox_Version.Location = new Point(255, 186);
-            comboBox_Version.Margin = new Padding(4, 4, 4, 4);
+            comboBox_Version.Margin = new Padding(4);
             comboBox_Version.Name = "comboBox_Version";
             comboBox_Version.Size = new Size(188, 33);
             comboBox_Version.TabIndex = 4;
@@ -156,7 +250,7 @@
             // 
             comboBox_Model.FormattingEnabled = true;
             comboBox_Model.Location = new Point(255, 144);
-            comboBox_Model.Margin = new Padding(4, 4, 4, 4);
+            comboBox_Model.Margin = new Padding(4);
             comboBox_Model.Name = "comboBox_Model";
             comboBox_Model.Size = new Size(188, 33);
             comboBox_Model.TabIndex = 3;
@@ -167,7 +261,7 @@
             comboBox_Constructor.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox_Constructor.FormattingEnabled = true;
             comboBox_Constructor.Location = new Point(255, 101);
-            comboBox_Constructor.Margin = new Padding(4, 4, 4, 4);
+            comboBox_Constructor.Margin = new Padding(4);
             comboBox_Constructor.Name = "comboBox_Constructor";
             comboBox_Constructor.Size = new Size(188, 33);
             comboBox_Constructor.TabIndex = 2;
@@ -188,7 +282,7 @@
             // 
             button_debug.Image = (Image)resources.GetObject("button_debug.Image");
             button_debug.Location = new Point(15, 678);
-            button_debug.Margin = new Padding(4, 4, 4, 4);
+            button_debug.Margin = new Padding(4);
             button_debug.Name = "button_debug";
             button_debug.Size = new Size(40, 38);
             button_debug.TabIndex = 4;
@@ -199,7 +293,7 @@
             // 
             button_Save.Image = Properties.Resources.diskette__2_;
             button_Save.Location = new Point(1616, 80);
-            button_Save.Margin = new Padding(4, 4, 4, 4);
+            button_Save.Margin = new Padding(4);
             button_Save.Name = "button_Save";
             button_Save.Size = new Size(40, 38);
             button_Save.TabIndex = 5;
@@ -228,100 +322,6 @@
             checkBox1.UseVisualStyleBackColor = true;
             checkBox1.CheckedChanged += checkBox1_CheckedChanged;
             // 
-            // tabControl_Parametre
-            // 
-            tabControl_Parametre.AccessibleName = "";
-            tabControl_Parametre.Controls.Add(tab_page_general);
-            tabControl_Parametre.Controls.Add(tabPage2);
-            tabControl_Parametre.Location = new Point(18, 283);
-            tabControl_Parametre.Name = "tabControl_Parametre";
-            tabControl_Parametre.SelectedIndex = 0;
-            tabControl_Parametre.Size = new Size(480, 283);
-            tabControl_Parametre.TabIndex = 12;
-            // 
-            // tab_page_general
-            // 
-            tab_page_general.Controls.Add(button1);
-            tab_page_general.Controls.Add(label1);
-            tab_page_general.Controls.Add(textBox1);
-            tab_page_general.Location = new Point(4, 34);
-            tab_page_general.Name = "tab_page_general";
-            tab_page_general.Padding = new Padding(3);
-            tab_page_general.Size = new Size(472, 245);
-            tab_page_general.TabIndex = 0;
-            tab_page_general.Text = "General";
-            tab_page_general.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            tabPage2.Controls.Add(button_Hostname);
-            tabPage2.Controls.Add(label_Hostname);
-            tabPage2.Controls.Add(textbox_Hostname);
-            tabPage2.Location = new Point(4, 34);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(472, 161);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // button_Hostname
-            // 
-            button_Hostname.Location = new Point(317, 18);
-            button_Hostname.Margin = new Padding(4);
-            button_Hostname.Name = "button_Hostname";
-            button_Hostname.Size = new Size(51, 36);
-            button_Hostname.TabIndex = 10;
-            button_Hostname.Text = "button1";
-            button_Hostname.UseVisualStyleBackColor = true;
-            button_Hostname.Click += button_Hostname_Click;
-            // 
-            // label_Hostname
-            // 
-            label_Hostname.AutoSize = true;
-            label_Hostname.Location = new Point(5, 23);
-            label_Hostname.Margin = new Padding(2, 0, 2, 0);
-            label_Hostname.Name = "label_Hostname";
-            label_Hostname.Size = new Size(102, 25);
-            label_Hostname.TabIndex = 8;
-            label_Hostname.Text = "Host Name";
-            // 
-            // textbox_Hostname
-            // 
-            textbox_Hostname.Location = new Point(116, 20);
-            textbox_Hostname.Margin = new Padding(2);
-            textbox_Hostname.Name = "textbox_Hostname";
-            textbox_Hostname.Size = new Size(188, 31);
-            textbox_Hostname.TabIndex = 9;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(382, 14);
-            button1.Margin = new Padding(4);
-            button1.Name = "button1";
-            button1.Size = new Size(51, 36);
-            button1.TabIndex = 13;
-            button1.Text = "Press";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(66, 20);
-            label1.Margin = new Padding(2, 0, 2, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(102, 25);
-            label1.TabIndex = 11;
-            label1.Text = "Host Name";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(185, 17);
-            textBox1.Margin = new Padding(2);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(188, 31);
-            textBox1.TabIndex = 12;
-            // 
             // MainWindows
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
@@ -338,13 +338,13 @@
             Text = "(";
             Load += Form1_Load;
             ConfigurationPanel.ResumeLayout(false);
+            tabControl_Parametre.ResumeLayout(false);
+            tabpage_General.ResumeLayout(false);
+            tabpage_General.PerformLayout();
+            tabPage_Vlan.ResumeLayout(false);
+            tabPage_Vlan.PerformLayout();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
-            tabControl_Parametre.ResumeLayout(false);
-            tab_page_general.ResumeLayout(false);
-            tab_page_general.PerformLayout();
-            tabPage2.ResumeLayout(false);
-            tabPage2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -367,11 +367,11 @@
         private Label label_Path;
         private CheckBox checkBox1;
         private TabControl tabControl_Parametre;
-        private TabPage tab_page_general;
-        private Button button1;
-        private Label label1;
-        private TextBox textBox1;
-        private TabPage tabPage2;
+        private TabPage tabpage_General;
+        private Button button_Vlan;
+        private Label label_Vlan;
+        private TextBox textBox_Vlan;
+        private TabPage tabPage_Vlan;
         private Button button_Hostname;
         private Label label_Hostname;
         private TextBox textbox_Hostname;
